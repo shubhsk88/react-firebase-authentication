5b9bdeacd8c8990685fed697fe7873516c2d22d8
Shubham Singh <shubham88singh@gmail.com>
2020-06-10 11:56:03 +0530

    Adding the authentication method

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,5 @@
 import app from 'firebase/app';
-
+import 'firebase/auth';
 const config = {
   apiKey: 'AIzaSyB-4jyPK_jM-1EPowLf3K8cjCl7im55UQo',
   authDomain: 'react-firebase-authentic-83597.firebaseapp.com',
@@ -14,7 +14,18 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+    this.auth = app.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+  doSignUserWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+  doSignOut = () => this.auth.signOut();
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
